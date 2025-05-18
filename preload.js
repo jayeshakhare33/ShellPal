37c044358625e102d3fb9bb135c02b579c248ca4
Jayesh Akhare <147245391+jayeshakhare33@users.noreply.github.com>
2025-05-18 08:46:54 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-// preload.js
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('api', {
-  sendInput: (data) => ipcRenderer.send('terminal-input', data),
-  onOutput: (callback) => ipcRenderer.on('terminal-output', (_event, data) => callback(data)),
-  onError: (callback) => ipcRenderer.on('terminal-error', (_event, data) => callback(data)),
-  onExit: (callback) => ipcRenderer.on('terminal-exit', (_event, code) => callback(code)),
-});
